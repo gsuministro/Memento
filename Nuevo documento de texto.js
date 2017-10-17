585edfe99d53d798e7bcea0e8012c2f18ec7a2b7
gsuministro <32876463+gsuministro@users.noreply.github.com>
2017-10-17 14:29:46 -0400

    Add files via upload

---
+++ Nuevo documento de texto.js
@@ -0,0 +1,7 @@
+<script>
+moment().format('MMMM Do YYYY, h:mm:ss a'); // April 3rd 2017, 5:09:39 pm
+moment().format('dddd');                    // Monday
+moment().format("MMM Do YY");               // Apr 3rd 17
+moment().format('YYYY [escaped] YYYY');     // 2017 escaped 2017
+moment().format();                          // 2017-04-03T17:09:39+02:00
+</script>
